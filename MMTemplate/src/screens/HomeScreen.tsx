72da7b88e304c2d11dc20af0e273bb5b08d1f2b6
Modha Manish <mmdigitalvichar@gmail.com>
2026-01-01 14:27:14 +0530

    feat: Implement animated logo display on home screen with theme-aware full-screen container.

--- MMTemplate/src/screens/HomeScreen.tsx
+++ MMTemplate/src/screens/HomeScreen.tsx
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet } from 'react-native';
 import React from 'react';
+import FullScreenContainer from '../components/FullScreenContainer';
+import { Images } from '../assets/images';
+import { ThemeType } from '../theme/Colors';
+import { mobileScreenHeight, mobileScreenWidth } from '../utils/utilsHelper';
+import { useTheme } from '../context/ThemeContext';
+import AnimationView from '../components/AnimationView';
 
 const HomeScreen = () => {
+  const theme = useTheme();
+  const styles = getStyles(theme);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <FullScreenContainer
+      edges={[]}
+      style={styles.container}
+      statusBarHidden
+      barStyle="light-content"
+    >
+      <AnimationView animType="FadeIn" duration={1000}>
+        <AnimationView animType="ZoomIn" duration={1000}>
+          <Image source={Images.logo} style={styles.logo} />
+        </AnimationView>
+      </AnimationView>
+    </FullScreenContainer>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const getStyles = ({ colors }: ThemeType) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colors.backgroundColor,
+    },
+    logo: {
+      alignSelf: 'center',
+      height: mobileScreenHeight,
+      width: mobileScreenWidth * 0.6,
+      resizeMode: 'contain',
+    },
+  });
